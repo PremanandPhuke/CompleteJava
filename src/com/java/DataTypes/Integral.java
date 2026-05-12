48c0f77deffcedc3dc119ae92c13f163c0738eea
PremanandPhuke <premanandphuke2002@gmail.com>
2026-05-12 11:04:10 +0530

    Created some packages for start now

---
+++ src/com/java/DataTypes/Integral.java
@@ -0,0 +1,7 @@
+package com.java.DataTypes;
+
+public class Integral {
+    public static void main(String[] args) {
+        System.out.println("Integral data types");
+    }
+}
---
+++ src/com/java/Operators/Conditional.java
@@ -0,0 +1,7 @@
+package com.java.Operators;
+
+public class Conditional {
+    public static void main(String[] args) {
+        System.out.println("conditional");
+    }
+}
