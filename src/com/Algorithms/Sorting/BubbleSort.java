204f9c75e069b69828a19b5ca9f33b98dc366caa
PremanandPhuke <premanandphuke2002@gmail.com>
2026-07-07 20:13:58 +0530

    Bubble sort

--- src/com/Algorithms/Sorting/BubbleSort.java
+++ src/com/Algorithms/Sorting/BubbleSort.java
@@ -1,4 +1,29 @@
 package com.Algorithms.Sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        int[] arr=  {12, 1, 23, 11, 24};
+        Bubble(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void Bubble(int[] arr){
+         for(int i=0; i < arr.length; i++){
+             for(int j=1; j < arr.length -i; j++){
+
+                 if(arr[j] < arr[j-1]){
+                     int temp = arr[j];
+                     arr[j]= arr[j-1];
+                     arr[j-1]= temp;
+
+                 }
+             }
+         }
+    }
+
 }
